8ed4f2463ec10fe8f97b65deb9bfb67c733da765
Clemens Auinger <clemensauinger@gmail.com>
2024-09-24 12:43:13 +0200

    finished PalindromeTester

--- POS_HUE2/src/Main.java
+++ POS_HUE2/src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         NumberTester numberTester = new NumberTester("testFile.txt");
-        numberTester.setPalindromeTester(number -> );
+        numberTester.setPalindromeTester(number -> {
+            StringBuilder stringBuilder = new StringBuilder(number);
+            stringBuilder.reverse();
+            return stringBuilder.equals(number);
+        });
+        numberTester.setOddEvenTester((number) -> number % 2 == 0);
         numberTester.setPrimeTester();
-        numberTester.setOddEvenTester();
+
+
     }
 }
