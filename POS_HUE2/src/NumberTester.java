bc1ecfd7838a1528dad213452115c839682e1e83
Clemens Auinger <clemensauinger@gmail.com>
2024-09-24 12:26:08 +0200

    implemented testFile method and finished NumberTester class

--- POS_HUE2/src/NumberTester.java
+++ POS_HUE2/src/NumberTester.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,7 +9,7 @@ public class NumberTester {
         private NumberTest oddTester;
         private NumberTest palindromeTester;
         private NumberTest primeTester;
-        Map<Integer,Integer> fileMap = new HashMap<>();
+        private Map<Integer,Integer> fileMap = new LinkedHashMap<>();
 
     private static String FILENAME;
     public NumberTester(String fileName){
@@ -28,6 +29,16 @@ public class NumberTester {
     }
 
     public void testFile(){
+        fileMap.forEach((key, value) -> {
+            switch (key){
+                case 1:
+                    System.out.println(oddTester.testNumber(value));
+                case 2:
+                    System.out.println(primeTester.testNumber(value));
+                case 3:
+                    System.out.println(palindromeTester.testNumber(value));
+            }
+        });
 
 
     }
@@ -40,7 +51,7 @@ public class NumberTester {
             System.out.println(e.getMessage());
         }
 
-
+        scanner.nextLine();
         while(scanner.hasNext()){
             String[] readLine = scanner.nextLine().split(" ");
             fileMap.put(Integer.parseInt(readLine[0]),Integer.parseInt(readLine[1]));
