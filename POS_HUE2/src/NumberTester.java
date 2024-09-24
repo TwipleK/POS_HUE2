45dbe38ace00016ec5720eb850750423603eece2
Clemens Auinger <clemensauinger@gmail.com>
2024-09-24 11:51:45 +0200

    Example 2: created and implemented readFile method and created and filled Map

--- POS_HUE2/src/NumberTester.java
+++ POS_HUE2/src/NumberTester.java
@@ -1,14 +1,26 @@
-public class NumberTester {
-    public NumberTester(String fileName){
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class NumberTester {
+        private NumberTest oddTester;
+        private NumberTest palindromeTester;
+        private NumberTest primeTester;
+        Map<Integer,Integer> fileMap = new HashMap<>();
+
+    private static String FILENAME;
+    public NumberTester(String fileName){
+        FILENAME = fileName;
     }
 
-    public void setOddEvenTester (NumberTester oddTester){
+    public void setOddEvenTester (NumberTest oddTester){
         this.oddTester = oddTester;
     }
 
     public void setPrimeTester(NumberTest primeTester){
-
+        this.primeTester = primeTester;
     }
 
     public void setPalindromeTester(NumberTest palindromeTester){
@@ -17,5 +29,23 @@ public class NumberTester {
 
     public void testFile(){
 
+
     }
+
+    public void readFile(){
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(FILENAME));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        while(scanner.hasNext()){
+            String[] readLine = scanner.nextLine().split(" ");
+            fileMap.put(Integer.parseInt(readLine[0]),Integer.parseInt(readLine[1]));
+        }
+    }
+
+
 }
