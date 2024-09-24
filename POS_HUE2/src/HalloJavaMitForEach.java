739243cfa87f9523a9fdd21441ec280f69fe10a8
Clemens Auinger <clemensauinger@gmail.com>
2024-09-24 11:21:14 +0200

    finished first Task

--- POS_HUE2/src/HalloJavaMitForEach.java
+++ POS_HUE2/src/HalloJavaMitForEach.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class HalloJavaMitForEach {
     public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>(List.of("a","b","c","d","e"));
+
+        for (String s : stringList) System.out.println(s);
+
+        stringList.forEach(System.out::println);
+
 
     }
 }
