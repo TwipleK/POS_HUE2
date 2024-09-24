09214403c22dc6205cf3c16a98cadf9992058b6e
Clemens Auinger <clemensauinger@gmail.com>
2024-09-24 12:33:29 +0200

    created Main class

--- POS_HUE2/src/Main.java
+++ POS_HUE2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        NumberTester numberTester = new NumberTester("testFile.txt");
+        numberTester.setPalindromeTester(number -> );
+        numberTester.setPrimeTester();
+        numberTester.setOddEvenTester();
     }
 }
